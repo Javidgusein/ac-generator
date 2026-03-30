1e4790813e5a669e8a8564d3270519cdb2fb0e06
Javidgusein <cavidhuseinov@gmail.com>
2026-03-31 03:21:57 +0400

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -107,7 +107,7 @@ ${processedUml}`;
         'X-Title': 'AC Generator'
       },
       body: JSON.stringify({
-        model: 'meta-llama/llama-4-maverick:free',
+        stepfun/step-3.5-flash:free
         temperature: 0.1,
         max_tokens: 8000,
         messages: [
